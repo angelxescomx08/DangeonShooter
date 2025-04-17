3a2292ee392088f7b16c37e0fc58b74ba22eb0a8
angelxescomx08 <69997011+angelxescomx08@users.noreply.github.com>
2025-04-16 18:29:12 -0600

    feat: explosion

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private GameObject explosionPrefab;
+    [SerializeField] private int maxHealth = 5;
+    SpriteRenderer spriteRenderer;
+    private int health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = maxHealth;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Arrow>())
+        {
+            TakeDamage(1);
+        }
+    }
+
+    private void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        }
+        else
+        {
+            // Start blinking
+            StartCoroutine(Blink(0.1f));
+        }
+    }
+
+    private IEnumerator Blink(float blinkTime)
+    {   
+        spriteRenderer.color = Color.red;
+        yield return new WaitForSeconds(blinkTime);
+        spriteRenderer.color = Color.white;
+    }
+}
